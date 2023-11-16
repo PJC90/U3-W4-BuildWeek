d93529247fe0219339f6834cc1f5a9a30226f536
SarahGuarneri <sarahguarneri97@icloud.com>
2023-11-16 10:37:13 +0100

    linkedin notizie showmore

--- u3-w4-buildweek/src/Components/RightSideBarHome.jsx
+++ u3-w4-buildweek/src/Components/RightSideBarHome.jsx
@@ -2,14 +2,15 @@ import { Button, Col, Container, ListGroup, Row } from "react-bootstrap"
 import { InfoSquareFill } from "react-bootstrap-icons"
 import Pubblicità from '../SidebarImgs/pubblicità.png';
 import { CaretDown } from "react-bootstrap-icons";
+import { CaretUp } from 'react-bootstrap-icons';
 import { useState } from "react";
 
 const RightSideBar = () => {
 
-    const [showButton, setShowButton] = useState(true)
+    const [showMore, setShowMore] = useState(true)
 
     const toggleShowButton = () => {
-        setShowButton(!showButton)
+        setShowMore(!showMore)
     }
 
     return (
@@ -25,14 +26,38 @@ const RightSideBar = () => {
                         <p className="m-0" style={{fontSize: '0.7em'}}>1 giorno fa</p>
                     </ListGroup.Item>
                     <ListGroup.Item className="border-0 pt-0">
-                        <p className="m-0" style={{fontSize: '0.9em'}}><strong>Esselunga ora compete con Tannico</strong></p>
+                        <p className="m-0" style={{fontSize: '0.9em'}}><strong>Effetto ATP Finals per Torino</strong></p>
                         <p className="m-0" style={{fontSize: '0.7em'}}>1 giorno fa</p>
                     </ListGroup.Item>
                     <ListGroup.Item className="border-0 pt-0">
-                        <p className="m-0" style={{fontSize: '0.9em'}}><strong>Esselunga ora compete con Tannico</strong></p>
+                        <p className="m-0" style={{fontSize: '0.9em'}}><strong>Le esperienze trainano il lusso</strong></p>
                         <p className="m-0" style={{fontSize: '0.7em'}}>1 giorno fa</p>
                     </ListGroup.Item>
-                <Button variant="light" style={{color: 'grey'}} onClick={toggleShowButton}>Show more <CaretDown /></Button>
+                    {
+                        showMore ? 
+                        <Button variant="light" style={{color: 'grey'}} onClick={toggleShowButton}>Show more <CaretDown /></Button>
+                        :
+                        <>
+                        <ListGroup.Item className="border-0 pt-0">
+                        <p className="m-0" style={{fontSize: '0.9em'}}><strong>Le Telco peggiorano ancora</strong></p>
+                        <p className="m-0" style={{fontSize: '0.7em'}}>1 giorno fa</p>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="border-0 pt-0">
+                        <p className="m-0" style={{fontSize: '0.9em'}}><strong>Manuale di critica costruttiva</strong></p>
+                        <p className="m-0" style={{fontSize: '0.7em'}}>1 giorno fa</p>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="border-0 pt-0">
+                        <p className="m-0" style={{fontSize: '0.9em'}}><strong>Ristorazione sotto la lente</strong></p>
+                        <p className="m-0" style={{fontSize: '0.7em'}}>1 giorno fa</p>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="border-0 pt-0">
+                        <p className="m-0" style={{fontSize: '0.9em'}}><strong>Guerra Israele-Hamas: le ultime notizie</strong></p>
+                        <p className="m-0" style={{fontSize: '0.7em'}}>1 giorno fa</p>
+                        </ListGroup.Item>
+                        <Button variant="light" style={{color: 'grey'}} onClick={toggleShowButton}>Show less <CaretUp /></Button>
+                        </>
+                    }
+                {/* <Button variant="light" style={{color: 'grey'}} onClick={toggleShowButton}>Show more <CaretDown /></Button> */}
                 {/* {showButton ? <></> : 
                     <ListGroup.Item className="border-0 pt-0">
                         <p className="m-0" style={{fontSize: '0.9em'}}><strong>Esselunga ora compete con Tannico</strong></p>
